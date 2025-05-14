08ea8f69596ae7c9d7154b45a0694d901127e42e
kethlen martins <kethlen.silva@sou.inteli.edu.br>
2025-05-14 16:38:50 -0300

    feat: add tailwind css to project

--- src/app-rust/src/main.rs
+++ src/app-rust/src/main.rs
@@ -6,5 +6,15 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    rsx! { "HotDog!" }
+    rsx! { 
+        document::Stylesheet {
+            href: asset!("/assets/tailwind.css")
+        }
+
+        div {
+            class: "p-4 bg-green-500 text-white text-xl rounded",
+            "Tailwind está funcionando!"
+        }
+    
+    }
 }
