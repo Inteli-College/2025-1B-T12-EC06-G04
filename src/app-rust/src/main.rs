895a3e642a8f0cca0242e85f9ee958bbd7b13c7d
Gustavo-daCosta <gustavo.costa@sou.inteli.edu.br>
2025-05-16 11:40:24 -0300

    feat: router flux

--- src/app-rust/src/main.rs
+++ src/app-rust/src/main.rs
@@ -1,10 +1,25 @@
 use dioxus::prelude::*;
+use dioxus_router::prelude::*;
+
+mod home;
+use home::Home;
 
 fn main() {
-    dioxus::launch(App);
+    dioxus::launch(|| {
+        rsx! { Router::<Route> {} }
+    });
 }
 
-#[component]
-fn App() -> Element {
-    rsx! { "HotDog!" }
+#[allow(non_snake_case)]
+pub fn Home() -> Element {
+    rsx! {
+        h1 { "Home Page" }
+        Link { to: "/another-page", "Vá para outra paǵina" }
+    }
+}
+
+#[derive(Routable, PartialEq, Clone, Debug)]
+enum Route {
+    #[route("/")]
+    Home {},
 }
