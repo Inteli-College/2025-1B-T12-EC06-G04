f21c87ea062eed71aa5180a63cd80a8797a56967
cucapcosta <cucapcosta@gmail.com>
2025-05-26 17:37:26 -0300

    feat: Cria função de rodar modelo em py

--- src/Yolo/YOLO-Det-Py/rodar_modelo_prod.py
+++ src/Yolo/YOLO-Det-Py/rodar_modelo_prod.py
@@ -1,6 +1,19 @@
 from ultralytics import YOLO
 
 def rodar_modelo(path, modelPath):
+    labelToName = {
+        0: "retracao",
+        1: "termica"}
     model = YOLO(modelPath)
-    results = model.predict(source=path, save=True, save_txt=True, save_conf=True, show=True)
-    
+    results = model.predict(source=path, show=True)
+    total = {0: 0, 1: 0}
+    for result in results:
+        labels = result.boxes.cls.tolist()
+        for label in labels:
+            total[label] += 1
+        print(labels)
+    print("Total de objetos detectados por classe:")
+    for label, count in total.items():
+        print(f"Classe {labelToName[label]}: {count} detectado(s)")
+    return total
+print(rodar_modelo("RunDataset", "best.pt"))
