ef8cd52a9838dd1004645855b06f4183957cac68
cucapcosta <cucapcosta@gmail.com>
2025-06-03 17:17:53 -0300

    feat: Tentativa de modelo melhor

--- src/Yolo/YOLO-Det-Py/yolo_train.py
+++ src/Yolo/YOLO-Det-Py/yolo_train.py
@@ -1,11 +1,17 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8m.pt")
+model = YOLO("best2.pt")
 
 model.train(
     data='Dataset/dataset.yaml',
     epochs=30,
     imgsz=224,
-    batch=32,
-    name="yolo_rachaduras"
+    batch=16,
+    warmup_epochs=3,
+    mosaic=1.0,
+    mixup=0.2,
+    cutmix=0.2,
+    hsv_h=0.015, hsv_s=0.7, hsv_v=0.4,
+    perspective=0.0008, shear=2.0,
+    name="yolo_rachaduras_recall"
 )
