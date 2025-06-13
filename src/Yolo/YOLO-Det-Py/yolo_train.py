99d186571c5ab418fae8dd3d165a5fcb76a5baa7
cucapcosta <cucapcosta@gmail.com>
2025-06-13 14:12:00 -0300

    feat: Novo modelo e resultados

--- src/Yolo/YOLO-Det-Py/yolo_train.py
+++ src/Yolo/YOLO-Det-Py/yolo_train.py
@@ -1,17 +1,11 @@
 from ultralytics import YOLO
 
-model = YOLO("best2.pt")
+model = YOLO("yolov8n.pt")
 
 model.train(
     data='Dataset/dataset.yaml',
-    epochs=30,
+    epochs=200,
     imgsz=224,
     batch=16,
-    warmup_epochs=3,
-    mosaic=1.0,
-    mixup=0.2,
-    cutmix=0.2,
-    hsv_h=0.015, hsv_s=0.7, hsv_v=0.4,
-    perspective=0.0008, shear=2.0,
-    name="yolo_rachaduras_recall"
+    name="reclasse_v8n",
 )
